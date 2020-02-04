428417516a8145d908c9add51b85661d06e60b83
Anvar Sono <kerimbekovich89@gmail.com>
2020-02-03 19:10:32 -0600

    test

--- src/main/java/TestC.java
+++ src/main/java/TestC.java
@@ -2,5 +2,6 @@ public class TestC {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("World");
+        System.out.println("Code");
     }
 }
